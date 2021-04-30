c8054d2942d7440065e186f7d6fff7a7a3346608
gracele246 <73254122+gracele246@users.noreply.github.com>
2021-04-29 23:10:03 -0700

    bubble sort

--- Classwork/minilab/app.py
+++ Classwork/minilab/app.py
@@ -62,7 +62,7 @@ def testingminilab():
     if request.method == 'POST':
         value = request.form['list']
         k = bubblesorting
-        g = k.bubbleSort(value)
+        g = k.g_original(value)
         list = k.bubbleSort(value)
     return render_template("/minilab/testpage.html", g=g, list=list)
     #return render_template("/minilab/testpage.html")
--- Classwork/minilab/testing.py
+++ Classwork/minilab/testing.py
@@ -5,10 +5,19 @@ class bubblesorting:
     def __init__(self):
         self._lst=[]
 
+    def g_original(g_test):
+        g_var1 =  g_test
+        return g_var1.split(',')
+
     def bubbleSort(value_1):
-        n = len(value_1)
-        value = [64, 34, 25, 12, 22, 11, 90]
-        #value = np.array(value_1)
+
+        n = len(value_1)-3
+        value_g = value_1
+        arr = value_g.split(',')
+        value = arr
+        #value = value_1
+
+
         #g_var1 = value_1[1]
         #g_var2 = value_1[2]
         #g_var3 = value_1[3]
@@ -27,4 +36,4 @@ class bubblesorting:
             # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
-        return n
+        return value
